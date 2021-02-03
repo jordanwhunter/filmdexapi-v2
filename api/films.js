b871c02dc14c71dac6294c3b80c91f40430c72b4
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-03 14:35:14 -0500

    Vercel setup for connecting to database and setting up GET route

---
+++ api/films.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const typeString = { type: String, trim: true };
+const typeReqString = { type: String, required: true, trim: true };
+const typeReqNumber = { type: Number, required: true };
+const typeReqBoolean = { type: Boolean, required: true };
+const typeReqDate = { type: Date, required: true, default: Date.now };
+
+const customDescriptionSchema = new mongoose.Schema({ uniqueTag: typeString });
+const keyFeaturesSchema = new mongoose.Schema({ feature: typeString });
+
+const dbUri = process.env.MONGODB_URI;
+
+let filmSchema = new mongoose.Schema({
+  brand: typeReqString,
+  name: typeReqString,
+  iso: typeReqNumber,
+  formatThirtyFive: typeReqBoolean,
+  formatOneTwenty: typeReqBoolean,
+  color: typeReqBoolean,
+  process: typeReqString,
+  staticImageUrl: typeReqString,
+  description: typeReqString,
+  customDescription: [customDescriptionSchema],
+  keyFeatures: [keyFeaturesSchema],
+  dateAdded: typeReqDate
+});
+
+let Film = mongoose.model('film', filmSchema);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbUri, {
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false
+    });
+    console.log('Connected To Your Mongo Database!')
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectDB();
+
+module.exports = (req, res) => {
+  if (req.method === 'GET') {
+    Film.find()
+    .then(people => {
+      res.status(200).json(people)
+    })
+    .catch(error => res.status(204).json(error.message))
+  }
+};
