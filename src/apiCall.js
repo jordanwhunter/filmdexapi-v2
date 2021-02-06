464817e79eb62bb84c6951925d2babc38be54c36
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-06 15:59:30 -0500

    Built readFilmTypes function and added handleResponse

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,2 +1,11 @@
 const api = '/api/films';
 
+const handleResponse = (res) => {
+  if (res.ok) return res.json();
+  else res.text().then((text) => Promise.reject(text));
+};
+
+export const readFilmTypes = () => {
+  return fetch(api).then(handleResponse);
+};
+
