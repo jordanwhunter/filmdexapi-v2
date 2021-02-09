620e008f2b5e03248105315b918a30da0516f993
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-09 12:58:24 -0500

    Skeleton of modal component with toggle feature

---
+++ src/Modal.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let showModal = false;
+  export let message;
+</script>
+
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0,0,0,0.8);
+  }
+
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background: #fff;
+  }
+</style>
+
+{#if showModal}
+  <section class='backdrop' on:click>
+    <div class='modal'>
+      <p>{message}</p>
+    </div>
+  </section>
+{/if}
