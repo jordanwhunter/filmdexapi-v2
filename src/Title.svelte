0fc1bc54b1ab0463a9388ea99847d6d0a0fdea9d
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-09 14:53:10 -0500

    Created Title component

---
+++ src/Title.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<style>
+  .italic {
+    font-style: italic;
+  }
+
+  .smaller {
+    font-size: small;
+  }
+
+  .underline {
+    text-decoration: underline;
+  }
+</style>
+
+<section>
+  <h2>
+    <span class='underline'>The Film API</span>
+    <br>
+    <span class='italic smaller'>
+      A RESTful Camera Film API <br>
+      Cataloging 130+ different 35mm / 120 format film types and counting!
+    </span>
+  </h2>
+</section>
