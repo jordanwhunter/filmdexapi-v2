b9abdaaaefa065a6782650c465b1d628569ce60c
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-09 15:29:55 -0500

    Created About component

---
+++ src/About.svelte
@@ -0,0 +1,45 @@
+<script>
+
+</script>
+
+<style>
+  .left-align {
+    text-align: left;
+  }
+
+  .smaller {
+    font-size: small;
+  }
+
+  .underline {
+    text-decoration: underline;
+  }
+</style>
+
+<section class='left-align'>
+  <h5>What is this?</h5>
+  <p class='smaller'>
+    This website provides a RESTful <span class='underline'>application programming interface</span> (API) to detailed objects related to photo camera film - both in 35mm format and 120 format. Using this website, you can consume information on camera film brands, a specific film's ISO, developing processes, key features, and more. 
+  </p>
+  <h5>What is an API?</h5>
+  <p class='smaller'>
+    An <span class='underline'>application programming interface</span> (API) is a computing interface that defines interactions between multiple software intermediaries. It defines the kinds of calls or requests that can be made, how to make them, the data formats that should be used, the conventions to follow, etc. In this case, the application is a database of camera film types, and the interface is a URL link. <br><br>
+    A RESTful API, or a <span class='underline'>representational state transfer</span> API, is one that conforms to the constraints of HTTP(S) parameters.
+  </p>
+  <h5>How much information is here?</h5>
+  <p class='smaller'>
+    The API is small, but growing! Currently, we've catalogued around 130 different types of applicable photo film. The items within our database include:
+  </p>
+  <ul class='smaller'>
+    <li>Brand Name</li>
+    <li>Film Model</li>
+    <li>ISO</li>
+    <li>Availability in 35mm</li>
+    <li>Availability in 120 format</li>
+    <li>Black & White / Color Specification</li>
+    <li>Photographic Processing Type</li>
+    <li>Image (500 x 500)</li>
+    <li>Film Description</li>
+    <li>Key Features</li>
+  </ul>
+</section>
