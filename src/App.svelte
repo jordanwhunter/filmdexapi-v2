e2f3e6b85baac8a8388fa04fdfff66b0d51ceb42
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-09 15:43:34 -0500

    Refactor

--- src/App.svelte
+++ src/App.svelte
@@ -48,16 +48,8 @@
     grid-gap: 8px;
   }
 
-  .italic {
-    font-style: italic;
-  }
-
   .left-align {
     text-align: left;
-  }
-
-  .lighter {
-    font-weight: lighter;
   }
 
   .smaller {
@@ -97,29 +89,7 @@
 </Modal>
 <main>
   <Title />
-  <!-- <section>
-      <h2>
-        <span class='underline'>The Film API</span>
-        <br>
-        <span class='italic smaller'>
-          A RESTful Camera Film API <br>
-          Cataloging 130+ different 35mm / 120 format film types and counting!
-        </span>
-      </h2>
-  </section> -->
   <API />
-  <!-- <section>
-    <h4 class='italic'>
-    Direct link to results: 
-      <a 
-        href='https://filmapi.vercel.app/api/films' 
-        target='_blank' 
-        rel='noreferrer noopener'
-      >
-        https://filmapi.vercel.app/api/films
-      </a>
-    </h4>
-  </section> -->
   <br />
   <section>
     <p class='smaller'>(Scroll for overview of API's contents)</p>
@@ -154,36 +124,11 @@
     </div>
   </section>
   <h4 class='underline left-align'>About</h4>
-  <About />
-    <!-- <h5>What is this?</h5>
-    <p class='smaller'>
-      This website provides a RESTful <span class='underline'>application programming interface</span> (API) to detailed objects related to photo camera film - both in 35mm format and 120 format. Using this website, you can consume information on camera film brands, a specific film's ISO, developing processes, key features, and more. 
-    </p>
-    <h5>What is an API?</h5>
-    <p class='smaller'>
-      An <span class='underline'>application programming interface</span> (API) is a computing interface that defines interactions between multiple software intermediaries. It defines the kinds of calls or requests that can be made, how to make them, the data formats that should be used, the conventions to follow, etc. In this case, the application is a database of camera film types, and the interface is a URL link. <br><br>
-      A RESTful API, or a <span class='underline'>representational state transfer</span> API, is one that conforms to the constraints of HTTP(S) parameters.
-    </p>
-    <h5>How much information is here?</h5>
-    <p class='smaller'>
-      The API is small, but growing! Currently, we've catalogued around 130 different types of applicable photo film. The items within our database include:
-    </p>
-    <ul class='smaller'>
-      <li>Brand Name</li>
-      <li>Film Model</li>
-      <li>ISO</li>
-      <li>Availability in 35mm</li>
-      <li>Availability in 120 format</li>
-      <li>Black & White / Color Specification</li>
-      <li>Photographic Processing Type</li>
-      <li>Image (500 x 500)</li>
-      <li>Film Description</li>
-      <li>Key Features</li>
-    </ul> -->
-  <section class='left-align'>
+  <div class='container left-align'>
+    <About />
     <h5>This API is missing film types that I'm familiar with!</h5>
     <p class='smaller'>
-      We're fully aware! While we feel like we've already made decent progress of growing our database, we're just getting started! Feel free to contribute to open issues on <a href='https://github.com/jordanwhunter/filmdexapi-v2' target='_blank'>Github</a>. If you aren't a developer, but would still like to contribute, please click <button on:click={toggleModal}>here</button>
+      We're fully aware! While we feel like we've already made decent progress of growing our database, we're just getting started! Feel free to contribute to open issues on <a href='https://github.com/jordanwhunter/filmdexapi-v2' target='_blank'>Github</a>. If you aren't a developer, but would still like to contribute, please click <button on:click={toggleModal}>here</button> please note: submitting a film type does not guarantee that it will be added to the API's database. 
     </p>
-  </section>
+  </div>
 </main>
