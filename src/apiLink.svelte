6b1be60d7ce185c7a3afa946594775856236f1f4
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-09 15:19:08 -0500

    Created API component

---
+++ src/apiLink.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<style>
+  .italic {
+    font-style: italic;
+  }
+</style>
+
+<section>
+  <h4 class='italic'>
+  Direct link to results: 
+    <a 
+      href='https://filmapi.vercel.app/api/films' 
+      target='_blank' 
+      rel='noreferrer noopener'
+    >
+      https://filmapi.vercel.app/api/films
+    </a>
+  </h4>
+</section>
